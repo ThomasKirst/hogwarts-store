7a605b1f98312e1458d41b0b6651eeae5b5db534
Thomas Kirst <thomas.kirst@neuefische.de>
2021-02-22 16:05:36 +0100

    Basic styling for product cards

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,18 +1,25 @@
+import styled from 'styled-components';
+
 export default function ProductCard({ product }) {
   console.log(product);
   return (
-    <>
+    <Card>
       <h4>{product.name}</h4>
-      <span>{product.price}</span>
-      <span>{product.currency}</span>
-      <span>{product.packageSize}</span>
-      <span>{product.supportContact}</span>
-      <div>
-        {product.tags.map((tag) => (
-          <span>{tag}</span>
-        ))}
-      </div>
-      <span>{product.tag}</span>
-    </>
+      <p>
+        <span>{product.price + ' '}</span>
+        <span>{product.currency}</span>
+      </p>
+      <p>Package Size: {product.packageSize}</p>
+      <p>{product.supportContact}</p>
+      <p>{product.tags}</p>
+    </Card>
   );
 }
+
+const Card = styled.div`
+  background-color: #6171ad;
+  padding: 0.5rem 0.5rem 2rem 1.5rem;
+  border-radius: 10px;
+  max-width: 500px;
+  margin: 2rem auto 0;
+`;
